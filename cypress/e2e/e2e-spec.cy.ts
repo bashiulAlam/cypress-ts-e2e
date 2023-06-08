8ceba73e213feeb766935f7377c4688437549c69
Bashiul Alam Sabab <bashiul.alam@gmail.com>
2023-06-08 17:31:59 +0200

    test added for adding one book to collection and deleting it

--- cypress/e2e/e2e-spec.cy.ts
+++ cypress/e2e/e2e-spec.cy.ts
@@ -1,13 +1,69 @@
 import bookPage from "../page-objects/bookPage";
 import userCredentials from "../fixtures/userCredentials.json";
+import testData from "../fixtures/testData.json";
 
 describe('E2E UI Tests', () => {
 
-  before(() => {
+  beforeEach(() => {
     bookPage.openHomePage();
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      return false;
+    });
+
     bookPage.login(userCredentials.username, userCredentials.password);
-    bookPage.validateLogin(userCredentials.username);
   });
 
-  it.skip('', () => {});
+  it('Add book to collection', () => {
+    bookPage.getDisplayedUsername().then((displayedName) => {
+      let displayName = displayedName.text();
+      cy.log('username displayed : ' + displayName);
+      expect(displayName.includes(userCredentials.username)).to.be.true;
+    });
+
+    bookPage.searchWithPublisher(testData.publisher);
+    bookPage.getPublisherNameFromSearchResult().each(($element, index) => {
+      let text = $element.text().trim();
+      if (text.length > 0 && index > 0) {
+        //cy.log('checking text ' + $element.text());
+        expect(text.includes(testData.publisher)).to.be.true;
+      }
+    });
+
+    let authorName : string, title : string;
+    bookPage.getAuthorNameFromSearchResult().each(($element, index) => {
+      if (index == testData.bookSerial) {
+        authorName = $element.text().trim();
+        cy.log('author name : ' + authorName);
+      }
+    });
+    bookPage.getTitleFromSearchResult().each(($element, index) => {
+      if (index == testData.bookSerial) {
+        title = $element.text().trim();
+        cy.log('book title : ' + title);
+      }
+    });
+
+    bookPage.openTitleBySerial(testData.bookSerial);
+    bookPage.getTitleFromDetailPage().then(($bookTitle) => {
+      expect($bookTitle.text().includes(title)).to.be.true;
+    });
+
+    bookPage.addBookToCollection();
+    bookPage.navigateToProfileCollection();
+    bookPage.getAuthorFromProfile().then(($author) => {
+      expect($author.text().includes(authorName)).to.be.true;
+    });
+    bookPage.getBookTitleFromProfile().then(($bookTitle) => {
+      expect($bookTitle.text().includes(title)).to.be.true;
+    });
+  });
+
+  it('Delete book from collection', () => {
+    bookPage.navigateToProfileCollection();
+    bookPage.deleteBookFromCollection();
+    cy.wait(3000);
+    bookPage.getBookTitleFromProfile().then(($bookTitle) => {
+      expect($bookTitle.text().trim()).to.be.empty;
+    });
+  });
 })
--- cypress/page-objects/bookPage.ts
+++ cypress/page-objects/bookPage.ts
@@ -10,8 +10,72 @@ class bookPage {
         cy.get('#login').click();
     }
 
-    validateLogin(username : string) {
-        cy.get('#userName-value').should('have.value', username);
+    getDisplayedUsername() {
+        return cy.get('#userName-value');
+    }
+
+    searchWithPublisher(publisher : string) {
+        cy.get('#searchBox').type(publisher);
+        cy.get('#basic-addon2').click();
+    }
+
+    getPublisherNameFromSearchResult() {
+        return cy.get('.rt-tr > :nth-child(4)');
+    }
+
+    getAuthorNameFromSearchResult() {
+        return cy.get('.rt-tr > :nth-child(3)');
+    }
+
+    getTitleFromSearchResult() {
+        return cy.get('.rt-tr > :nth-child(2)');
+    }
+
+    openTitleBySerial(serial : number) {
+        let locator = ':nth-child(index) > .rt-tr > :nth-child(2) a';
+        locator = locator.replace('index', String(serial));
+        cy.get(locator).click();
+    }
+
+    getTitleFromDetailPage() {
+        return cy.get('#title-wrapper > .col-md-9 > #userName-value');
+    }
+
+    addBookToCollection() {
+        cy.contains('button', 'Add To Your Collection').click({force : true});
+        cy.on('window:alert', (text) => {
+            expect(text).to.contain('Book added to your collection.');
+        });
+    }
+
+    validateBookFromProfileCollection(title : string, authorName : string) {
+        cy.get(':nth-child(6) > .element-list > .menu-list > #item-3').click();
+        cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(2)').then(($bookTitle) => {
+            expect($bookTitle.text()).to.contain(title);
+        });
+        cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(3)').then(($author) => {
+            expect($author.text()).to.contain(authorName);
+        });
+    }
+
+    navigateToProfileCollection() {
+        cy.get(':nth-child(6) > .element-list > .menu-list > #item-3').click();
+    }
+
+    getBookTitleFromProfile() {
+        return cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(2)');
+    }
+
+    getAuthorFromProfile() {
+        return cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(3)');
+    }
+
+    deleteBookFromCollection() {
+        cy.get('#delete-record-undefined').click();
+        cy.get('#closeSmallModal-ok').click();
+        cy.on('window:alert', (text) => {
+            expect(text).to.contain('Book deleted.');
+        });
     }
 }
 
