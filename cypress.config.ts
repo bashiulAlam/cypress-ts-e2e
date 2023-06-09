31e0b8ca4290ea0a09e84983d9407e619219eb36
Bashiul Alam Sabab <bashiul.alam@gmail.com>
2023-06-09 10:50:05 +0200

    mochaawesome reporter added

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,17 @@ module.exports = defineConfig({
   defaultCommandTimeout: 8000,
   watchForFileChanges: false,
   chromeWebSecurity: false,
+  retries: {
+    runMode: 1,
+  },
+  reporter: 'mochawesome',
+  "reporterOptions": {
+    "html": true,
+    "json": true,
+    "reportDir": "cypress/reports",
+    "reportFilename": "report",
+    "overwrite": true 
+  },
   e2e: {
     baseUrl: 'https://demoqa.com/books'
   },
